50e813744631ebc93ebd0438aae10fb78246e141
William Borges <36491847+Will-Borges@users.noreply.github.com>
2023-09-04 21:04:10 -0300

    subindo ponto e virgula

--- Desenvolvimento/AMXCurrentAccount/Presenters/Interfaces/ICurrentAccountPresenter.cs
+++ Desenvolvimento/AMXCurrentAccount/Presenters/Interfaces/ICurrentAccountPresenter.cs
@@ -1,12 +1,11 @@
 ﻿namespace AMXCurrentAccount.Presenters.Interfaces
 {
-    using AMXCurrentAccount.Core.Domain.CurrentAccount.Models.Response.GetCustomerCurrentAccount;
     using AMXCurrentAccount.Views.GetCurrentAccount.Response;
     using AMXCurrentAccount.Views.PostCustomerCurrentAccount.Request;
 
     public interface ICurrentAccountPresenter
     {
         Task PostCustomerCurrentAccount(CustomerCurrentAccountRequestDTO customerCurrentAccount);
-        Task<CustomerCurrentAccountResponseDTO> GetCustomerCurrentAccount(int customerId)
+        Task<CustomerCurrentAccountResponseDTO> GetCustomerCurrentAccount(int customerId);
     }
 }
